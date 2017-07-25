5441a2a1c4ba3d92621e43fa9ea9529eb400905f
olivar3 <olivar3@outlook.com>
2017-07-25 16:05:05 -0500

    modified app js file

--- most-wanted-starter-code-master/app.js
+++ most-wanted-starter-code-master/app.js
@@ -6,15 +6,17 @@ Build all of your functions for displaying and gathering information below (GUI)
 function app(people){
   var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
   switch(searchType){
-    case 'yes':{
+    case 'yes':
       searchByName(people);
     break;
+
     case 'no':
-    // TODO: search by traits
+      searchByTrait(people);
+    
     break;
-    default:
     app(people); // restart app
     break;
+
   }
 }
 
@@ -45,31 +47,55 @@ function mainMenu(person, people){
     break;
     case "quit":
     return; // stop execution
-    default:
+    default: "Go back to Main Menu"
     return mainMenu(person, people); // ask again
   }
 }
 
 function searchByName(people){
-  var firstName = promptFor("What is the person's first name?", chars);
-  var lastName = promptFor("What is the person's last name?", chars);
-  firstName = data.data.length.firstName;
-  lastName = data.[data.length].lastName;
+ var firstName = promptFor("What is the person's first name?", chars);
+ var lastName = promptFor("What is the person's last name?", chars);
+for(var i = 0; i < people.length; i++){
+  if(firstName === people[i].firstName && lastName === people.[i].lastName){
+    displayPerson(people[i])
+  }
+   else{
+
+    alert("could not find individual")
+   }
+}
+//This may continue to loop over and over and will not have and end, could be bronken witb a break.
+ // firstName = data.data.length.firstName;  
+ // lastName = data.[data.length].lastName;
+
+ //iterate over people array of objects and look where firstname === people[2].firstname
+ //if there's a match, then return that person
 }
 function searchByTrait(people){
-  var gender = promptFor("What is the person's gender?", chars);
-  var dob =  promptFor("what is the person's dob?"), chars;
-  var height =  promptFor("what is the person's height?"), chars;
-  var weight =  promptFor("what is the person's weight?"), chars;
-  var eyeColor =  promptFor("what is the person's eyeColor?"), chars;
-  var occupation =  promptFor("what is the person's occupation?"), chars;
-  gender = data.data.lenght.gender;
-  dob = data.data.lenght.dob;
-  height  = data.data.lenght.height;
-  weight = data.data.lenght.weight;
-  eyeColor = data.data.lenght.eyeColor;
-  occupation = data.data.lenght.occupation;
-}
+var gender = promptFor("What is the person's gender?", chars);
+var dob =  promptFor("what is the person's dob?"), chars;
+var height =  promptFor("what is the person's height?"), chars;
+var weight =  promptFor("what is the person's weight?"), chars;
+var eyeColor =  promptFor("what is the person's eyeColor?"), chars;
+var occupation =  promptFor("what is the person's occupation?"), chars;
+gender.map(function gender{[people.length]
+  return gender;
+  });
+dob.map(function dob{[people.length]
+  return dob;
+  });
+height.map(function height{[people.length]
+  return height;
+  });
+weight.map(function weight{[people.length]}
+  return weight;
+  });
+eyeColor.map(function eyeColor{[people.length]
+ return eyeColor;
+ });
+occupation.map(function occupation{[people.length]
+  return occupation;
+ });
 
 // alerts a list of people
 function displayPeople(people){
@@ -79,12 +105,20 @@ function displayPeople(people){
 }
 
 function displayPerson(person){
-  // print all of the information about a person:
-  // height, weight, age, name, occupation, eye color.
-  var personInfo = "First Name: " + person.firstName + "\n";
-  personInfo += "Last Name: " + person.lastName + "\n";
-  // TODO: finish getting the rest of the information to display
-  alert(personInfo);
+ // print all of the information about a person:
+ // height, weight, age, name, occupation, eye color.
+ var personInfo = "first Name: " + person.firstName + "\n";
+ personInfo += "Last Name: " + person.lastName + "\n";
+ personInfo += "gender:" + persons.gender + "\n";
+ personInfo += "height:" + person.height + "\n";
+ personInfo += "eyeColor:" + person.eyeColor + "\n";
+ personInfo += "weight:" + person.weight + "\n";
+ personInfo += "occupation:" + person.occupation + "\n";
+ personInfo += "dob:" + person.dob + "\n";
+ personInfo += "id:" + person.id + "\n";
+ personInfo += "parents:" + person.parents + "\n";
+ personInfo += "curentSpouse:" + person.curentSpouse + "\n";
+ alert(personInfo);
 }
 
 // function that prompts and validates user input
